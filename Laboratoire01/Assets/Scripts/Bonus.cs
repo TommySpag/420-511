b918467e3306941237e7119f2e4328955f2be565
TommySpag <tommyspagnuolo@gmail.com>
2024-09-03 11:38:30 -0400

    Add project files.

---
+++ Laboratoire01/Assets/Scripts/Bonus.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonus : MonoBehaviour
+{
+    // Initiate variables
+    private float speed = 20.0f;
+    private float forwardInput;
+    private bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        if (active)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (forwardInput > 0)
+        {
+            active = true;
+        }
+        else if (forwardInput < 0)
+        {
+            active = false;
+        }
+    }
+}
